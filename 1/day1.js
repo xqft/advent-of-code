d7c1b6ba06bfcd8a1101bb1aefc342a1ee2dc5c1
xqft <estefano.bargas@fing.edu.uy>
2022-11-22 22:36:50 -0300

    Update to day 1

--- 1/day1.js
+++ 1/day1.js
@@ -4,6 +4,6 @@ const isGreater = (elem, index, arr) => index !== 0 && elem > arr[index-1];
 
 const result = input
   .map(isGreater)
-  .reduce((acc, elem) => acc + (elem? 1 : 0), 0);
+  .reduce((acc, elem) => acc + elem, 0); // true == 1, false == 0.
 
 console.log(result);
