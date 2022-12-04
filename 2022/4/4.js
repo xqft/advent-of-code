c28f0be0ed1b9823f448f108108c8a29cda9332a
xqft <estefano.bargas@fing.edu.uy>
2022-12-04 13:53:31 -0300

    Day 4 part two

--- 2022/4/4.js
+++ 2022/4/4.js
@@ -13,3 +13,15 @@ const fully_contained_count = sections
   .length
 
 console.log(fully_contained_count);
+
+
+/* ======== Part two ======== */
+
+
+const overlapping_count = sections
+  .split('\n')
+  .map(pair => pair.replace(',', '-').split('-').map(Number))
+  .filter(([a, b, c, d]) => !((b < c) || (a > d)))
+  .length
+
+console.log(overlapping_count);
