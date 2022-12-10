862e939bf4c3b0d7c5a2f7c3b09dcfb577076f6b
xqft <estefano.bargas@fing.edu.uy>
2022-12-10 13:41:47 -0300

    Day 8 part two

--- 2022/8/8.js
+++ 2022/8/8.js
@@ -23,3 +23,38 @@ console.log(matrix
   .map((row, i) => row.filter((col, j) => visible(matrix, i, j)))
   .flat()
   .length);
+
+
+/* ======== Part two ======== */
+
+
+Array.prototype.endWhen = function(lambda) {
+  const end = this.findIndex(lambda) + 1 || this.length;
+  return this.slice(0, end);
+}
+
+Array.prototype.endWhenRight = function(lambda) {
+  return this.reverse().endWhen(lambda).reverse();
+}
+
+function scenic_score(matrix, row, col) {
+  const row_arr = matrix[row];
+  const col_arr = matrix.map(row => row[col]);
+
+  const taller = (height) => height >= matrix[row][col];
+
+  const visible_sides = [
+    row_arr.slice(0, col) .endWhenRight(taller),  // left
+    row_arr.slice(col + 1).endWhen(taller),       // right
+    col_arr.slice(0, row) .endWhenRight(taller),  // top
+    col_arr.slice(row + 1).endWhen(taller),       // bottom
+  ];
+
+  return visible_sides
+    .map(arr => arr.length)
+    .reduce((acc, curr) => acc * curr);
+}
+
+console.log(Math.max(...matrix
+  .map((row, row_index) => row.map((_, col_index) => scenic_score(matrix, row_index, col_index)))
+  .flat()));
